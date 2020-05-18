5ddb8391734c4a749e1edc97d31d464d3763e554
Magnus Buk <info@magnusbuk.de>
2020-05-18 08:45:08 +0200

    Highlight current day

--- module/Calendar/src/Calendar/View/Helper/DateRow.php
+++ module/Calendar/src/Calendar/View/Helper/DateRow.php
@@ -17,17 +17,14 @@ class DateRow extends AbstractHelper
             $view->dateFormat($date, IntlDateFormatter::FULL)));
         $dateFormat = $view->dateFormat($date, IntlDateFormatter::LONG);
 
-        /*
-        $isActive = $date->setTime(0, 0, 0)
-            ->diff(DateTime::createFromFormat('Y-m-d', time())
+        $dateDiff = $date->setTime(0, 0, 0)
+            ->diff(DateTime::createFromFormat('Y-m-d', date('Y-m-d'))
                 ->setTime(0, 0, 0));
-        */
-        $isActive = false;
 
         return sprintf(
             '<tr class="calendar-date-row %s"><td class="%s" colspan="%s"><div class="day-label">%s</div><div class="date-label">%s</div></td></tr>',
             $outerClasses,
-            ($isActive) ? 'active' : '',
+            (0 === $dateDiff->days) ? 'active' : '',
             $colspan,
             $dayName,
             $dateFormat);
